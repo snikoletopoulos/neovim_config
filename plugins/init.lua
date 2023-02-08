6c2536fc93f360cdfe4b34fa77899b859b570799
Stavros <snikoletopoulos@hotmail.com>
2023-02-08 02:27:28 +0200

    Format auto save definition

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,13 +1,7 @@
 return {
     ["github/copilot.vim"] = {},
     ["bkad/CamelCaseMotion"] = {},
-    ["pocco81/auto-save.nvim"] = {
-        config = function()
-            require("auto-save").setup({
-                debounce_delay = 1000,
-            })
-        end
-    },
+    ["pocco81/auto-save.nvim"] = { config = function() require("auto-save").setup({ debounce_delay = 1000, }) end },
     ["folke/trouble.nvim"] = { requires = "nvim-tree/nvim-web-devicons" },
     ["styled-components/vim-styled-components"] = {},
     ["hail2u/vim-css3-syntax"] = {},
