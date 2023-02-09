4db78fb172b4aa496d135daee5291b499f81202d
Stavros <snikoletopoulos@hotmail.com>
2023-02-09 18:50:42 +0200

    Change bufferline style

--- init.lua
+++ init.lua
@@ -12,6 +12,21 @@ local config = {
             window = {
                 width = 40
             }
+        },
+        bufferline = {
+            options = {
+                separator_style = "padded_slant",
+                diagnostics = "nvim_lsp",
+                diagnostics_indicator = function(count, level)
+                    local icon = level:match("error") and " " or " "
+                    return " " .. icon .. count
+                end,
+                hover = {
+                    enabled = true,
+                    delay = 0.5,
+                    reveal = { "close" }
+                },
+            }
         }
     },
     highlights = {
