170c0d32c947aebcb1307fbcd24e5169cec87c12
Stavros <snikoletopoulos@hotmail.com>
2023-02-11 16:21:51 +0200

    Add missing keymaps descriptions

---
+++ which-key/register.lua
@@ -0,0 +1,21 @@
+return {
+    n = {
+        Y = "Yank line",
+        ["<leader>"] = {
+            name = "leader",
+            w = "Next camel case word",
+            b = "Previous camel case word",
+            e = "Next end of camel case word",
+            ge = "Previous end of camel case word",
+        }
+    },
+    i = {
+        ["<S-Left>"] = "Previous camel case word",
+        ["<S-Right>"] = "Next camel case word",
+    },
+    v = {
+        g = {
+            r = "Selection"
+        }
+    },
+}
