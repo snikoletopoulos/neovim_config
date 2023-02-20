0f3a7dcfbf093377731d1b89e95f48c6a56eb359
Stavros <snikoletopoulos@hotmail.com>
2023-02-20 02:24:52 +0200

    Remove comments

--- init.lua
+++ init.lua
@@ -28,16 +28,6 @@ return {
       foldlevel = 99,
     },
   },
-  -- heirline = {
-  --   icon_highlights = {
-  --     breadcrumbs = true,
-  --     file_icon = {
-  --       tabline = function(self) return self.is_active or self.is_visible end,
-  --       statusline = true,
-  --       winbar = true,
-  --     },
-  --   }
-  -- }
   -- plugins = {
   --   -- TODO
   --   cmp = {
