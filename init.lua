1c2ed923618165a2d28f61ee8ffa95af619e9dc8
Stavros <snikoletopoulos@hotmail.com>
2023-02-20 10:30:23 +0200

    Use bufferline plugin

--- init.lua
+++ init.lua
@@ -17,7 +17,6 @@ return {
       camelcasemotion_key = "<leader>",
       copilot_no_tab_map = true,
       loaded_perl_provider = 0,
-      heirline_bufferline = true,
       sonokai_style = "andromeda",
     },
     opt = {
