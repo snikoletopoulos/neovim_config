34965fd4ec86865af054ecdb6fa171b80135687e
Stavros <snikoletopoulos@hotmail.com>
2023-06-04 00:04:31 +0300

    Don't load lazily targets.vim

--- plugins/motion.lua
+++ plugins/motion.lua
@@ -1,6 +1,6 @@
 return {
   { "bkad/CamelCaseMotion", lazy = false },
-  { "wellle/targets.vim",   lazy = "VeryLazy" },
+  { "wellle/targets.vim",   lazy = false },
   {
     "kylechui/nvim-surround",
     event = "VeryLazy",
