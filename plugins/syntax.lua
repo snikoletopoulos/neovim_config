2988bd4aebaa2625cef94276785bc8f09f69da08
Stavros <snikoletopoulos@hotmail.com>
2023-06-29 03:29:20 +0300

    Add indent-blackline plugin

--- plugins/syntax.lua
+++ plugins/syntax.lua
@@ -28,4 +28,11 @@ return {
       }
     },
   },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    dependencies = { "TheGLander/indent-rainbowline.nvim" },
+    opts = function(_, opts)
+      return require("indent-rainbowline").make_opts(opts, { color_transparency = 0.05 })
+    end,
+  }
 }
