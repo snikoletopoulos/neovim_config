3f40d7d0e9db76a92df74f1e0a40c1cd58025281
Stavros <snikoletopoulos@hotmail.com>
2023-08-24 10:48:10 +0300

    Clean mappings

--- mappings.lua
+++ mappings.lua
@@ -3,8 +3,6 @@ local maps = { n = {}, i = {}, v = {}, x = {}, o = {} }
 -- Editing
 maps.n.n = { "nzzzv", desc = "Next result" }
 maps.n.N = { "Nzzzv", desc = "Previous result" }
-maps.x["J"] = { ":m '>+1<CR>gv=gv", desc = "Move line(s) up" }
-maps.x["K"] = { ":m '<-2<CR>gv=gv", desc = "Move line(s) up" }
 maps.x["<leader>p"] = { "\"_dP", desc = "Paste without coping" }
 maps.n["<leader>y"] = { "\"+y", desc = "Yank to clipboard" }
 maps.x["<leader>y"] = { "\"+y", desc = "Yank to clipboard" }
@@ -29,21 +27,18 @@ maps.n["<leader>uu"] = false -- undotree.nvim
 maps.n["<leader>ft"] = false -- todo-comments.nvim
 maps.n["<leader>uh"] = false -- harpoon
 maps.n["<leader>e"] = false  -- CamelCaseMotion
+maps.n["<leader>w"] = false  -- CamelCaseMotion
 maps.n["<leader>ud"] = false -- neo-tree-diagnostics.nvim
 
 -- Just naming
-maps.n.Y = { name = "Yank line" }
-maps.v.g = { r = { name = "Selection" } }
-maps.n["<leader>s"] = { name = " Search" }
-maps.x["<leader>s"] = { name = " Search" }
-maps.n["<leader>h"] = { name = "󱡅 Harpoon" }
+maps.n["<leader>h"] = { name = "󱡅 Harpoon", desc = "󱡅 Harpoon" }
 
 -- CamelCaseMotion
-maps.n["<leader>w"] = { desc = "Next camel case word" }
-maps.v["<leader>w"] = { desc = "Next camel case word" }
-maps.v["<leader>e"] = { desc = "Next end of camel case word" }
-maps.v["<leader>b"] = { desc = "Next start of camel case word" }
-maps.n["<leader>ge"] = { desc = "Previous end of camel case word" }
-maps.v["<leader>ge"] = { desc = "Previous end of camel case word" }
+-- maps.n["<leader>w"] = { desc = "Next camel case word" }
+-- maps.v["<leader>w"] = { desc = "Next camel case word" }
+-- maps.v["<leader>e"] = { desc = "Next end of camel case word" }
+-- maps.v["<leader>b"] = { desc = "Next start of camel case word" }
+-- maps.n["<leader>ge"] = { desc = "Previous end of camel case word" }
+-- maps.v["<leader>ge"] = { desc = "Previous end of camel case word" }
 
 return maps
