b28bdddeac8470467a7610a6463bec7e6aca62d3
Stavros <snikoletopoulos@hotmail.com>
2023-08-25 10:22:41 +0300

    Remove duplicate mappings

--- plugins/motion.lua
+++ plugins/motion.lua
@@ -4,10 +4,8 @@ return {
     lazy = false, -- TODO: remove this when keymaps are fixed
     keys = {
       { "<leader>w",  "<Plug>CamelCaseMotion_w",  desc = "Next camel case word",            mode = { "n", "v" } },
-      { "<leader>w",  "<Plug>CamelCaseMotion_w",  desc = "Next camel case word",            mode = { "n", "v" } },
       { "<leader>e",  "<Plug>CamelCaseMotion_e",  desc = "Next end of camel case word",     mode = { "n", "v" } },
       { "<leader>b",  "<Plug>CamelCaseMotion_b",  desc = "Next start of camel case word",   mode = { "n", "v" } },
-      { "<leader>ge", "<Plug>CamelCaseMotion_ge", desc = "Previous end of camel case word", mode = { "n", "v" } },
       { "<leader>ge", "<Plug>CamelCaseMotion_ge", desc = "Previous end of camel case word", mode = { "n", "v" } },
     }
   },
