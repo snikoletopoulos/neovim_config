a105de6e5f6f4d3e07a6cada7b854a01ec59e229
Stavros <snikoletopoulos@hotmail.com>
2023-09-21 10:40:59 +0300

    Remove unnessesary option

--- init.lua
+++ init.lua
@@ -4,7 +4,6 @@ return {
       camelcasemotion_key = "<leader>",
       copilot_no_tab_map = true,
       loaded_perl_provider = 0,
-      sonokai_style = "andromeda",
       maplocalleader = ",",
     },
     opt = {
