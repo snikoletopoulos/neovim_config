be09459362d9a7c37a8dd938f8d0cec5608e4bcd
Stavros <snikoletopoulos@hotmail.com>
2023-10-23 15:23:31 +0300

    Add name as per upgrade manual

--- plugins/syntax.lua
+++ plugins/syntax.lua
@@ -30,6 +30,7 @@ return {
   },
   {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     dependencies = { "TheGLander/indent-rainbowline.nvim" },
     opts = function(_, opts)
       return require("indent-rainbowline").make_opts(opts, { color_transparency = 0.05 })
