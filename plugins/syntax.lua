027a420bfe894624974f88aa7f5811e3ff189d1f
Stavros <snikoletopoulos@hotmail.com>
2024-01-03 02:09:49 +0200

    Load indent-blackline when entering a file

--- plugins/syntax.lua
+++ plugins/syntax.lua
@@ -27,6 +27,7 @@ return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		main = "ibl",
+		-- event = "User AstroFile",
 		dependencies = { "TheGLander/indent-rainbowline.nvim" },
 		opts = function(_, opts)
 			return require("indent-rainbowline").make_opts(
