aefd0ecc247bf423538961e21f6671936816c3f0
Stavros <snikoletopoulos@hotmail.com>
2024-01-05 03:01:41 +0200

    Remove unused events

--- plugins/navigation.lua
+++ plugins/navigation.lua
@@ -89,10 +89,6 @@ return {
 				},
 			},
 			event_handlers = {
-				-- TODO: Hide cursor when entering neotree
-				-- { event = "neo_tree_buffer_enter",       handler = function() vim.cmd 'highlight! Cursor blend=100' end },
-				-- cspell:disable-next
-				-- { event = "neo_tree_buffer_leave",       handler = function() vim.cmd 'highlight! Cursor guibg=#5f87af blend=0' end },
 				{
 					event = neo_tree_events.NEO_TREE_WINDOW_AFTER_OPEN,
 					handler = function()
