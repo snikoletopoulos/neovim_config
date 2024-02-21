4f3d1ebf69b3e6493099a2a4af23edf863944ae7
Stavros <snikoletopoulos@hotmail.com>
2024-02-21 02:54:12 +0200

    Fix disabling LSP keymaps

---
+++ lsp/mappings.lua
@@ -0,0 +1,6 @@
+local maps = { n = {}, i = {}, v = {}, x = {}, o = {}, c = {} }
+
+maps.n["<leader>uY"] = false -- toggle LSP semantic highlighting
+maps.v["<leader>uY"] = false -- toggle LSP semantic highlighting
+
+return maps
