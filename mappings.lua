ee4b01fc68219081937ce732b0091161d5c25f51
Stavros <snikoletopoulos@hotmail.com>
2024-03-16 22:13:46 +0200

    Keep Keymaps class consistant

--- mappings.lua
+++ mappings.lua
@@ -1,33 +1,5 @@
----@alias Modes "n"|"i"|"v"|"x"|"o"|"c",
-
----@class Keymaps
----@field n table
----@field i table
----@field v table
----@field x table
----@field o table
----@field c table
----@field new fun(self: Keymaps, o?: Keymaps): Keymaps
----@field add fun(self: Keymaps, mode: Modes, lhs: string, value: table|false): nil
----@field create fun(self: Keymaps): table
-local Keymaps = { n = {}, i = {}, v = {}, x = {}, o = {}, c = {} }
-
-function Keymaps:new(o)
-	local k = o or {}
-	setmetatable(k, self)
-	self.__index = self
-	return k
-end
-
-function Keymaps:add(mode, lhs, value)
-	self[mode][lhs] = value
-end
-
-function Keymaps:create()
-	return { n = self.n, i = self.i, v = self.v, x = self.x, o = self.o, c = self.c }
-end
-
-local maps = Keymaps:new()
+---@type Keymaps
+local maps = astronvim.user_opts("classes.Keymaps"):new()
 
 maps:add("n", "<leader>h", { name = "󱡅 Harpoon", desc = "󱡅 Harpoon" })
 
