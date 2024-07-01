cc4ba449b77899730c015e17c57f8fabf67470c3
Stavros <snikoletopoulos@hotmail.com>
2024-07-02 02:28:07 +0300

    Hide cursor when in neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -99,6 +99,14 @@ return {
 						event = neo_tree_events.NEO_TREE_WINDOW_AFTER_CLOSE,
 						handler = function() vim.cmd("wincmd =") end,
 					},
+					{
+						event = "neo_tree_buffer_enter",
+						handler = function() vim.cmd("highlight! Cursor blend=100") end,
+					},
+					{
+						event = "neo_tree_buffer_leave",
+						handler = function() vim.cmd("highlight! Cursor guibg=#5f87af blend=0") end,
+					},
 				},
 			})
 		end,
