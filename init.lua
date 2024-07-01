fe77ddcdecee2800cc26627cdd6e027dfe51e33b
Stavros <snikoletopoulos@hotmail.com>
2024-07-02 02:01:02 +0300

    Fix missspelled words

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
-local lazypath = vim.env.LAZY or vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+local lazy_path = vim.env.LAZY or vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazy_path)) then
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazy_path })
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(lazy_path)
 
 -- validate that lazy is available
 if not pcall(require, "lazy") then
   -- stylua: ignore
-  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazy_path), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
 	vim.fn.getchar()
 	vim.cmd.quit()
 end
--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -5,7 +5,7 @@ require("lazy").setup({
 		import = "astronvim.plugins",
 		opts = { -- AstroNvim options must be set here with the `import` key
 			mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
-			maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
+			maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up spell-checker:disable-line
 			icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
 			pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
 		},
@@ -13,15 +13,15 @@ require("lazy").setup({
 	{ import = "community" },
 	{ import = "plugins" },
 } --[[@as LazySpec]], {
-	install = { colorscheme = { require("colorscheme"), "astrodark", "habamax" } },
+	install = { colorscheme = { require("colorscheme"), "astrodark", "habamax" } }, -- spell-checker:disable-line
 	ui = { backdrop = 40 },
 	performance = {
 		rtp = {
 			disabled_plugins = {
 				"gzip",
-				"netrwPlugin",
+				"netrwPlugin", -- spell-checker:disable-line
 				"tarPlugin",
-				"tohtml",
+				"tohtml", -- spell-checker:disable-line
 				"zipPlugin",
 			},
 		},
