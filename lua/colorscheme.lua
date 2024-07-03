e3847d9733ee7d05f46e62a2d33a94578d9f0da1
Stavros <snikoletopoulos@hotmail.com>
2024-07-03 09:57:12 +0300

    Add themes and change default

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -2,10 +2,10 @@ local favorites = {
 	tokyonight = "tokyonight-night",
 	one = "one",
 	sonokai = "sonokai",
-	edge = "edge",
-	github = "github_dark_colorblind",
 	astronvim = "astrodark",
-	vscode = "vscode",
+	ayu = "ayu",
+	synthweave = "synthweave",
+	onedarkpro = "onedark_dark",
 }
 
-return favorites.sonokai
+return favorites.onedarkpro
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -11,18 +11,25 @@ return {
 			vim.g.sonokai_float_style = "dim"
 			vim.g.sonokai_diagnostic_virtual_text = "colored"
 			vim.g.sonokai_better_performance = 1
+			vim.g.sonokai_inlay_hints_background = "dimmed"
 		end,
 	},
-	{
-		"sainnhe/edge",
-		init = function()
-			vim.g.edge_style = "aura"
-			vim.g.edge_dim_inactive_windows = 1
-			vim.g.edge_diagnostic_virtual_text = "colored"
-			vim.g.edge_diagnostic_line_highlight = "colored"
-		end,
-	},
-	{ "projekt0n/github-nvim-theme" },
 	{ "AstroNvim/astrotheme" },
-	{ "Mofiqul/vscode.nvim" },
+	{
+		"Shatur/neovim-ayu",
+		opts = { mirage = false },
+		config = function(_, opts) require("ayu").setup(opts) end,
+	},
+	{ "samharju/synthweave.nvim" },
+	{
+		"olimorris/onedarkpro.nvim",
+		opts = {
+			styles = {
+				types = "bold",
+				methods = "bold",
+				comments = "italic",
+				keywords = "italic",
+			},
+		},
+	},
 }
