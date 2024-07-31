852b7fdad1c45a5227de2cc7f47f793b82907c75
Stavros <snikoletopoulos@hotmail.com>
2024-08-01 01:54:12 +0300

    Use which-key to name groups

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -5,9 +5,11 @@ require("keymaps.disable"):configure(maps, { astronvim = true, packages = true, 
 require("keymaps.editing"):configure(maps)
 if not vim.g.neovide then require("keymaps.neovide"):configure(maps) end
 
-maps:add("n", "<Leader>h", { desc = "󱡅 Harpoon" })
-maps:add("n", "<Leader>c", { desc = " Log / 󰭹 Copilot Chat" })
-maps:add("v", "<Leader>c", { desc = "󰭹 Copilot Chat" })
-maps:add({ "n", "v" }, "<Leader>cc", { desc = "󰭹 Copilot Chat" })
+require("which-key").add({
+	{ "<leader>h", group = "󱡅 Harpoon", mode = "n" },
+	{ "<leader>c", group = " Log / 󰭹 Copilot Chat", mode = "n" },
+	{ "<leader>c", group = "󰭹 Copilot Chat", mode = "v" },
+	{ "<leader>cc", group = "󰭹 Copilot Chat", mode = { "n", "v" } },
+})
 
 return maps
