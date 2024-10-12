16f32458490fd522c6a36de2fa543a3602c57115
Stavros <snikoletopoulos@hotmail.com>
2024-10-12 15:50:11 +0200

    Add plugin for markdown rendering

--- lua/plugins/editor-behavior.lua
+++ lua/plugins/editor-behavior.lua
@@ -79,16 +79,21 @@ return {
 	},
 	{
 		"OXY2DEV/markview.nvim",
-		opts = {
-			modes = { "n", "i", "no", "c" },
-			hybrid_modes = { "i" },
-			callbacks = {
-				on_enable = function(_, win)
-					vim.wo[win].conceallevel = 2
-					vim.wo[win].concealcursor = "nc"
-				end,
-			},
-			headings = { shift_width = 2 },
-		},
+		opts = function()
+			local presets = require("markview.presets")
+
+			return {
+				modes = { "n", "i", "no", "c" },
+				hybrid_modes = { "i" },
+				callbacks = {
+					on_enable = function(_, win)
+						vim.wo[win].conceallevel = 2
+						vim.wo[win].concealcursor = "nc"
+					end,
+				},
+				headings = { shift_width = 2 },
+				horizontal_rules = presets.horizontal_rules.thin,
+			}
+		end,
 	},
 }
