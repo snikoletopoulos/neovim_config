4423b68cfebe9798276aa4f9bd1d97bff2c9b425
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2024-12-31 13:23:31 +0100

    Test action caching

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -66,10 +66,11 @@ return {
 			show_help = true,
 			window = { relative = "win" },
 			mappings = { reset = { normal = "<C-r>", insert = "<C-r>" } },
-		},
+			},
 		config = function(_, opts)
 			require("CopilotChat").setup(opts)
 
+			-- TODO: make markdown plugins treat copilot buffer as markdown
 			vim.api.nvim_create_autocmd("BufEnter", {
 				pattern = "copilot-*",
 				callback = function()
@@ -84,4 +85,44 @@ return {
 		enabled = vim.env.CHAT_GPT_API_KEY,
 		opts = { api_key_cmd = "echo " .. (vim.env.CHAT_GPT_API_KEY or "") },
 	},
+	-- {
+	-- 	"yetone/avante.nvim",
+	-- 	event = "VeryLazy",
+	-- 	-- lazy = false,
+	-- 	build = "make",
+	-- 	dependencies = {
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		"stevearc/dressing.nvim",
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+	-- 		"zbirenbaum/copilot.lua", -- for providers='copilot'
+	-- 		{
+	-- 			"HakonHarnes/img-clip.nvim",
+	-- 			event = "VeryLazy",
+	-- 			opts = {
+	-- 				default = {
+	-- 					embed_image_as_base64 = false,
+	-- 					prompt_for_file_name = false,
+	-- 					drag_and_drop = {
+	-- 						insert_mode = true,
+	-- 					},
+	-- 					use_absolute_path = true,
+	-- 				},
+	-- 			},
+	-- 		},
+	-- 		{
+	-- 			-- Make sure to set this up properly if you have lazy=true
+	-- 			"MeanderingProgrammer/render-markdown.nvim",
+	-- 			opts = {
+	-- 				file_types = { "markdown", "Avante" },
+	-- 			},
+	-- 			ft = { "markdown", "Avante" },
+	-- 		},
+	-- 	},
+	-- 	opts = {
+	-- 		provider = "openai",
+	-- 		auto_suggestions_provider = "copilot",
+	-- 	},
+	-- },
 }
