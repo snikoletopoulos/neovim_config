5180a9e00db15b3204cb6d196e97de30ee0dc553
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-01-06 04:01:13 +0100

    Fix cmdline popup menu height

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,37 +4,17 @@ return {
 	dependencies = { "folke/snacks.nvim" },
 	opts = function()
 		return {
-			lsp = { progress = { enabled = true } },
 			views = {
 				cmdline_popup = {
 					border = Snacks.util.is_transparent() and {} or {
 						style = "none",
 						padding = { 1, 2 },
 					},
-					filter_options = {},
-					win_options = {
-						winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-					},
-				},
-				popupmenu = {
-					relative = "editor",
-					position = {
-						row = 27,
-						col = "50%",
-					},
-					size = {
-						width = 60,
-						height = 10,
-					},
-					border = {
-						style = "rounded",
-						padding = { 0, 1 },
-					},
 				},
 			},
 			presets = {
 				bottom_search = false,
-				command_palette = false,
+				command_palette = true,
 			},
 		}
 	end,
