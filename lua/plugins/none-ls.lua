1123ec544e8f3374968ec0c453168f69615972f4
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-02-01 06:32:58 +0100

    Add formatting/diagnostics for gradle and groovy files

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,6 +7,7 @@ return {
 	},
 	opts = function(_, opts)
 		local none_ls = require("null-ls")
+		local utils = require("null-ls.utils").make_conditional_utils()
 
 		opts.sources = require("astrocore").extend_tbl(opts.sources, {
 			-- Code Actions
@@ -21,24 +22,21 @@ return {
 			none_ls.builtins.diagnostics.dotenv_linter.with({ filetype = { "dotenv" } }),
 			none_ls.builtins.diagnostics.hadolint, -- Linter for Docker
 			none_ls.builtins.diagnostics.ktlint, -- Linter for Kotlin
-			-- none_ls.builtins.diagnostics.jsonlint,
 			none_ls.builtins.diagnostics.hadolint, -- Linter for docker
 			none_ls.builtins.diagnostics.markdownlint,
+			none_ls.builtins.diagnostics.npm_groovy_lint,
 			none_ls.builtins.diagnostics.mypy,
 			none_ls.builtins.diagnostics.pylint,
 			none_ls.builtins.diagnostics.selene.with({
 				extra_args = function()
-					local utils = require("null-ls.utils").make_conditional_utils()
 					local has_project_config = utils.root_has_file({ ".selene.toml", "selene.toml" })
 					if has_project_config then return nil end
 					return { "--config", vim.fn.stdpath("config") .. "/config_files/.selene.toml" }
 				end,
 			}), -- Linter for Lua
-			-- none_ls.builtins.diagnostics.spectral, -- JSON/YAML linter
 			none_ls.builtins.diagnostics.sqlfluff.with({
 				extra_args = function()
 					local args = { "--dialect", "postgres" }
-					local utils = require("null-ls.utils").make_conditional_utils()
 					local has_project_config = utils.root_has_file({
 						"setup.cfg",
 						"tox.ini",
@@ -67,6 +65,7 @@ return {
 			none_ls.builtins.formatting.ktlint, -- Formatter for Kotlin
 			none_ls.builtins.formatting.markdownlint,
 			none_ls.builtins.formatting.nginx_beautifier,
+			none_ls.builtins.formatting.npm_groovy_lint, -- Formatter for Groovy/Gradle
 			none_ls.builtins.formatting.prettierd.with({
 				env = {
 					PRETTIERD_DEFAULT_CONFIG = vim.fn.stdpath("config") .. "/config_files/.prettierrc.json",
@@ -76,8 +75,7 @@ return {
 			none_ls.builtins.formatting.shfmt.with({ extra_filetypes = { "zsh" } }), -- Formatter for Bash
 			none_ls.builtins.diagnostics.stylelint.with({
 				extra_args = function()
-					local null_ls_utils = require("null-ls.utils").make_conditional_utils()
-					local has_project_config = null_ls_utils.root_has_file({
+					local has_project_config = utils.root_has_file({
 						".stylelintrc.js",
 						"stylelint.config.js",
 						".stylelintrc.mjs",
@@ -103,8 +101,7 @@ return {
 			}),
 			none_ls.builtins.formatting.stylua.with({
 				extra_args = function()
-					local null_ls_utils = require("null-ls.utils").make_conditional_utils()
-					local has_project_config = null_ls_utils.root_has_file({
+					local has_project_config = utils.root_has_file({
 						".stylua.toml",
 						"stylua.toml",
 						".editorconfig",
