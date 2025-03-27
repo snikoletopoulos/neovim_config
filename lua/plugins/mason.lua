d93825b12813b487ce53897c70a0656cb5534f9f
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-03-27 22:13:26 +0100

    Add mason ensure_installed list

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,16 +4,27 @@ return {
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		opts = {
 			ensure_installed = {
+				-- language servers
+				"gradle-language-server",
+				"graphql-language-service-cli",
+				"rust-analyzer",
+
+				-- none-ls
 				"checkmake",
+				"clang-format",
 				"cspell",
 				"markdownlint",
 				"mypy",
+				"npm-groovy-lint",
 				"pylint",
 				"stylelint",
+				"sql-formatter",
+				"yamllint",
 
 				-- install any other package
-				-- "tree-sitter-cli",
+				"tree-sitter-cli",
 			},
+			auto_update = true,
 		},
 	},
 }
