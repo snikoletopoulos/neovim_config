758437f24a9460f00850db9049116a8559eb6d88
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-04-01 01:16:56 +0200

    Remove default config

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -1,10 +1,5 @@
 ---@type LazySpec
 return {
-	-- TODO: test default
-	-- {
-	-- 	"brenoprata10/nvim-highlight-colors",
-	-- 	opts = { enable_tailwind = true, enabled_named_colors = true },
-	-- },
 	{
 		"chrisgrieser/nvim-chainsaw",
 		keys = {
