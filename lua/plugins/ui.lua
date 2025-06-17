aa1b3de32400b1af6480e7adb1ded2de413709e5
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-06-18 01:05:49 +0200

    Revert community options

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -37,9 +37,9 @@ return {
 					signature = { enabled = true },
 					hover = { enabled = false },
 					override = {
-						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-						["vim.lsp.util.stylize_markdown"] = true,
-						["cmp.entry.get_documentation"] = true,
+						["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+						["vim.lsp.util.stylize_markdown"] = false,
+						["cmp.entry.get_documentation"] = false,
 					},
 				},
 				views = {
@@ -50,7 +50,13 @@ return {
 						},
 					},
 				},
-			}
+				---@type NoicePresets
+				presets = {
+					bottom_search = false,
+					command_palette = false,
+					inc_rename = false,
+				},
+			})
 		end,
 	},
 	{
