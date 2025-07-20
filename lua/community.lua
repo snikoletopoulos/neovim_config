6907d0444f2ddfb9d22d3f3cccee3b932487dbd8
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-07-20 17:58:50 +0300

    Remove chatgpt plugin

--- lua/community.lua
+++ lua/community.lua
@@ -13,7 +13,6 @@ return {
 
 	{ import = "astrocommunity.editing-support.auto-save-nvim" },
 	{ import = "astrocommunity.editing-support.cloak-nvim" }, -- Hide env variables
-	{ import = "astrocommunity.editing-support.chatgpt-nvim" },
 	{ import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
 	{ import = "astrocommunity.editing-support.treesj" }, -- Fold/Unfold code blocks
 
