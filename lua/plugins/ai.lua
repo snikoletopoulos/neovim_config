d29fc80a4fe9c517bb19e94d0c5186efdd3755c1
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-08-01 02:46:05 +0300

    Format code

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -7,9 +7,7 @@ return {
 			suggestion = { enabled = false },
 			panel = { enabled = false },
 			server_opts_overrides = {
-				settings = {
-					telemetry = { telemetryLevel = "off" },
-				},
+				settings = { telemetry = { telemetryLevel = "off" } },
 			},
 		},
 	},
