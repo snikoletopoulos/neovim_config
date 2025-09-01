a29d7155ad3609eaf949f6e325baffd8a14b128d
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-09-02 01:52:37 +0200

    Change mini.move keybindins to not conflict with aerospace

--- lua/plugins/editor-behavior.lua
+++ lua/plugins/editor-behavior.lua
@@ -135,4 +135,19 @@ return {
 		},
 		opts = {},
 	},
+	{
+		"nvim-mini/mini.move",
+		opts = {
+			mappings = {
+				left = "<C-M-h>",
+				right = "<C-M-l>",
+				down = "<C-M-j>",
+				up = "<C-M-k>",
+				line_left = "<C-M-h>",
+				line_right = "<C-M-l>",
+				line_down = "<C-M-j>",
+				line_up = "<C-M-k>",
+			},
+		},
+	},
 }
