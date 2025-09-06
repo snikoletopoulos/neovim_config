e8b473887c577a5557c00056e8fbab79fc94d5f6
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-09-06 10:54:02 +0200

    Add tab support to supermaven

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -19,6 +19,25 @@ return {
 	-- 			clear_suggestion = "<M-r>",
 	-- 		},
 	-- 	},
+	-- 	specs = {
+	-- 		{
+	-- 			"AstroNvim/astrocore",
+	-- 			opts = {
+	-- 				options = {
+	-- 					g = {
+	-- 						-- set the ai_accept function
+	-- 						ai_accept = function()
+	-- 							local suggestion = require("supermaven-nvim.completion_preview")
+	-- 							if suggestion.has_suggestion() then
+	-- 								vim.schedule(function() suggestion.on_accept_suggestion() end)
+	-- 								return true
+	-- 							end
+	-- 						end,
+	-- 					},
+	-- 				},
+	-- 			},
+	-- 		},
+	-- 	},
 	-- },
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
